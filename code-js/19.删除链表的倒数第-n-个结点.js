0e957b7f5fb1ee63f182325a8bca168123bc1dd4
Haitao Lee <lihaitaodesign@gmail.com>
2022-06-14 10:32:50 +0800

    feat: 删除链表的倒数第 N 个结点

---
+++ code-js/19.删除链表的倒数第-n-个结点.js
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=19 lang=javascript
+ *
+ * [19] 删除链表的倒数第 N 个结点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    var dummy = new ListNode()
+    dummy.next = head
+
+    var fast = dummy
+    var slow = dummy
+
+    while (n != 0) {
+        fast = fast.next
+        n--
+    }
+
+    while (fast.next) {
+        fast = fast.next
+        slow = slow.next
+    }
+
+    slow.next = slow.next.next
+    
+    return dummy.next
+};
+// @lc code=end
+
