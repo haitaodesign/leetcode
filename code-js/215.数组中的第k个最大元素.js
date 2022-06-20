6c6f2599326c350875eec700aa9cd131a69ae06c
Haitao Lee <lihaitaodesign@gmail.com>
2022-06-20 11:49:03 +0800

    feat: 数组中的第K个最大元素

---
+++ code-js/215.数组中的第k个最大元素.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=215 lang=javascript
+ *
+ * [215] 数组中的第K个最大元素
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    var sorted = nums.sort((a, b) => {
+        return b - a
+    })
+    return sorted[k -1]
+};
+// @lc code=end
+
