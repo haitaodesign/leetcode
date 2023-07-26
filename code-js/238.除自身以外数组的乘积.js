f42152e59841e35873fa74ca148b44d776302f5b
Haitao Lee <lihaitaodesign@gmail.com>
2023-07-26 17:17:42 +0800

    feat: [238] 除自身以外数组的乘积

---
+++ code-js/238.除自身以外数组的乘积.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=238 lang=javascript
+ *
+ * [238] 除自身以外数组的乘积
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    const length = nums.length;
+    let light = [];
+    let right = [];
+    let answer = [];
+
+    light[0] = 1;
+    for (let i = 0; i < length; i++) {
+        light[i] = nums[i-1] * light[i-1] 
+        
+    }
+
+    right[length - 1] = 1
+    for (let i = length - 2; i >=0 ; i--) {
+        right[i] = nums[i + 1] * right[i + 1]
+    }
+
+    for (let i = 0; i < length; i++) {
+        answer[i] = light[i] * right[i]
+    }
+    return answer;
+    
+};
+// @lc code=end
+
