cf329e7b0e89f20d405b29222d04691eb608a2a8
Haitao Lee <lihaitaodesign@gmail.com>
2023-10-09 17:30:08 +0800

    feat: [49] 字母异位词分组

---
+++ code-js/49.字母异位词分组.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=49 lang=javascript
+ *
+ * [49] 字母异位词分组
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const map = new Map()
+    for (let str of strs) {
+        let array = Array.from(str)
+        array.sort()
+        let key = array.toString()
+        let list = map.get(key) ? map.get(key) : new Array()
+        list.push(str)
+        map.set(key, list)
+    }
+    return Array.from(map.values())
+};
+// @lc code=end
+
