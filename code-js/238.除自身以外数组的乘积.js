fc87da94b42a79b5131f2a7402ac31cda6a35db7
Haitao Lee <lihaitaodesign@gmail.com>
2024-02-04 10:27:18 +0800

    feat: [238] 除自身以外数组的乘积 O(1)

--- code-js/238.除自身以外数组的乘积.js
+++ code-js/238.除自身以外数组的乘积.js
@@ -10,24 +10,40 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    const length = nums.length;
-    let light = [];
-    let right = [];
-    let answer = [];
+    // 1. 空间复杂度 O(N)
+    // const length = nums.length;
+    // let light = [];
+    // let right = [];
+    // let answer = [];
 
-    light[0] = 1;
-    for (let i = 1; i < length; i++) {
-        light[i] = nums[i-1] * light[i-1] 
+    // light[0] = 1;
+    // for (let i = 1; i < length; i++) {
+    //     light[i] = nums[i-1] * light[i-1] 
         
+    // }
+
+    // right[length - 1] = 1
+    // for (let i = length - 2; i >=0 ; i--) {
+    //     right[i] = nums[i + 1] * right[i + 1]
+    // }
+
+    // for (let i = 0; i < length; i++) {
+    //     answer[i] = light[i] * right[i]
+    // }
+
+    // 2. 空间复杂度 O(1)
+    const length = nums.length
+    const answer = []
+
+    answer[0] = 1
+    for (let i = 1; i < length; i++) {
+        answer[i] = nums[i - 1] * answer[i - 1]
     }
 
-    right[length - 1] = 1
-    for (let i = length - 2; i >=0 ; i--) {
-        right[i] = nums[i + 1] * right[i + 1]
-    }
-
-    for (let i = 0; i < length; i++) {
-        answer[i] = light[i] * right[i]
+    let right = 1
+    for (let i = length - 1; i >= 0; i--) {
+        answer[i] = answer[i] * right
+        right *= nums[i]
     }
     return answer;
     
